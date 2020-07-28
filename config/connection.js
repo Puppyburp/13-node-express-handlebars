31ac205af64d8ce0c5a6f6cfd38b501f36308056
Jennifer Walason <jenny@Jennifers-MBP.internal>
2020-07-28 08:41:31 -0400

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // set up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "",
+  password: "coffeepot",
   database: "burgers_db"
-});
+  });
+};
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
 
 // make connection
 connection.connect(function(err) {
