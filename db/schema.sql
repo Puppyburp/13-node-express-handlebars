6ea36a837d22f9bbc4e985e7ad492156ba97729e
Jennifer Walason <jenny@Jennifers-MBP.internal>
2020-07-28 17:38:02 -0400

    schema edit

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+-- DROP DATABASE IF EXISTS burgers_db;
+-- CREATE DATABASE burgers_db;
 
-USE burgers_db;
+-- USE burgers_db;
 CREATE TABLE burger (
-    id INT auto_increment NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255) NOT NULL, 
+    createdAt TIMESTAMP NOT NULL,
     devoured BOOLEAN NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
