03eafa108a3eeeb9b30803512701a3736aa27300
Jennifer Walason <jenny@Jennifers-MBP.internal>
2020-07-28 21:20:56 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -24,13 +24,7 @@ if (process.env.JAWSDB_URL) {
 
 
 // make connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // export connection for ORM to use
 module.exports = connection;
